6a192e28036a5f79d33da770cdbd70905efa902a
jonathan.seow <jonathan.seow@bytedance.com>
2024-10-05 09:04:41 +0800

    finish project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
